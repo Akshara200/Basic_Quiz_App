6ab8105cefddb81c3435eeb118d7405be1d964d3
Akshara200 <86549637+Akshara200@users.noreply.github.com>
2024-04-06 15:56:57 +0530

    Add files via upload

---
+++ QuestionAnswer.java
@@ -0,0 +1,39 @@
+package com.example.quiz_app;
+
+public class QuestionAnswer {
+
+    public static String question []={
+            "What is 150+326 ?",
+            "Who invented Telephone?",
+            "what is 24*16 ?",
+            "who is the founder of SpaceX?",
+            "In the given options, which is the Example of System Software?",
+            "What is the name of earth's natural satellite?",
+            "The theory of evolution was proposed by?",
+            "Which scientist proposed the three laws of motion?",
+
+    };
+    public static String choices [][]={
+            {"460" , "471" , "476" , "478"},
+            {"Graham Bell" , "Einstein" , "Edison" , "None of the above"},
+            {"96" , "384" , "102" , "108"},
+            {"Jeff Bezos" , "Elon Musk" , "Steve Jobs" , "Bill Gates"},
+            {"Windows" , "Linux" , "MacOS" , "All of the above"},
+            {"Moon", "Sun" , "Mars" , "venus"},
+            {"Stephen Hawking" , "Charles Darwin" , "Alexander Fleming" , "Francesco Redi"},
+            {"Thomas Alva Edison" , "Albert Einstein" , "Stephen Hawking" , "Isaac Newton"},
+
+
+
+    };
+    public static String correctAnswers []= {
+            "476",
+            "Graham Bell",
+            "384",
+            "Elon Musk",
+            "All of the above",
+            "Moon",
+            "Charles Darwin",
+            "Isaac Newton",
+    };
+}
